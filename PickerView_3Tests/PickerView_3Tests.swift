fea170d51f94a810293a4487a3eaa741a4aeae99
seongsinhye <sinhye2428@naver.com>
2025-01-21 15:45:23 +0900

    Initial Commit

---
+++ PickerView_3Tests/PickerView_3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  PickerView_3Tests.swift
+//  PickerView_3Tests
+//
+//  Created by 성신혜 on 1/21/25.
+//
+
+import Testing
+@testable import PickerView_3
+
+struct PickerView_3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ PickerView_3UITests/PickerView_3UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  PickerView_3UITestsLaunchTests.swift
+//  PickerView_3UITests
+//
+//  Created by 성신혜 on 1/21/25.
+//
+
+import XCTest
+
+final class PickerView_3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
